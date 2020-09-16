179624d703bb53ec517b061bc4a7c68183a07c88
eskinders17 <22ehaileyesus@athenian.org>
2020-09-16 15:18:44 +0000

    almost finished

--- my_code.py
+++ my_code.py
@@ -11,16 +11,16 @@ def factorial_calc(num):   #you may choose the name of the parameter
         print("The factorial of 0 is 1.")   
     else:
         for x in range(1, num + 1):
-            factorial = x + 1     
+            factorial = x * 1     
     return x    # be sure to return the factorial
 
 
 if __name__ == '__main__':
 
-    num = input("Enter number: ")
+    num = int(input("Enter number: "))
+    print(factorial_calc(num))
     # Test your code with this first
     # Change the argument to try different values
-    print(factorial_calc(num))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
     #num = input("Enter value to factorialize: ")
